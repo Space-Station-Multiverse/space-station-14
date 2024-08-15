e0f74f8b1278cf6017803ebcfd55d0a3625ba426
Skye <22365940+Skyedra@users.noreply.github.com>
2024-08-14 17:21:54 -0700

    MV Auth: Support HWID Matching for auto-migration

--- Content.Server/Database/ServerDbManager.cs
+++ Content.Server/Database/ServerDbManager.cs
@@ -185,6 +185,7 @@ namespace Content.Server.Database
         Task<PlayerRecord?> GetPlayerRecordByUserName(string userName, CancellationToken cancel = default);
         Task<PlayerRecord?> GetPlayerRecordByUserId(NetUserId userId, CancellationToken cancel = default);
         Task<PlayerRecord?> GetPlayerRecordByPublicKey(ImmutableArray<byte> publicKey, CancellationToken cancel = default);
+        Task<PlayerRecord?> GetPlayerRecordByHWID(ImmutableArray<byte> hwID, CancellationToken cancel = default);
         #endregion
 
         #region Connection Logs
@@ -553,6 +554,12 @@ namespace Content.Server.Database
         {
             DbReadOpsMetric.Inc();
             return RunDbCommand(() => _db.GetPlayerRecordByPublicKey(publicKey, cancel));
+        }
+
+        public Task<PlayerRecord?> GetPlayerRecordByHWID(ImmutableArray<byte> hwID, CancellationToken cancel = default)
+        {
+            DbReadOpsMetric.Inc();
+            return RunDbCommand(() => _db.GetPlayerRecordByHWID(hwID, cancel));
         }
 
         public Task<int> AddConnectionLogAsync(
